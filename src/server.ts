918f282b5ac75b6c2e3046bdbe81d5e208597699
eduardoe6 <edueduardo0699@gmail.com>
2021-03-10 11:40:33 -0300

    Adicionando rota estática para que seja possível visualizar o avatar diretamente no navegador.

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import 'reflect-metadata';
+import uploadConfig from './config/upload';
 import './database';
 import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.listen(3333, () => {
