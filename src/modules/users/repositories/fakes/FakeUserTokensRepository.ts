a3908894ffca65c6c36c2f801b45d1bf4e6a07f4
eduardoe6 <edueduardo0699@gmail.com>
2021-03-30 17:29:17 -0300

    Criando testes unitários e services para funcionalidade de reset de senha.

--- src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
+++ src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -8,9 +8,23 @@ class FakeUserTokensRepository implements IUserTokensRepository {
   public async generate(userId: string): Promise<UserToken> {
     const userToken = new UserToken();
 
-    Object.assign(userToken, { id: v4(), token: v4(), userId });
+    Object.assign(userToken, {
+      id: v4(),
+      token: v4(),
+      userId,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
 
     this.userTokens.push(userToken);
+
+    return userToken;
+  }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = this.userTokens.find(
+      findToken => findToken.token === token,
+    );
 
     return userToken;
   }
---
+++ src/modules/users/services/ResetPasswordService.ts
@@ -0,0 +1,52 @@
+import IHashProvider from '@modules/users/providers/HashProvider/models/IHashProvider';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+import AppError from '@shared/errors/AppError';
+import { addHours, isAfter } from 'date-fns';
+import { inject, injectable } from 'tsyringe';
+
+interface IRequestDTO {
+  token: string;
+  password: string;
+}
+
+@injectable()
+class ResetPasswordService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
+
+    @inject('HashProvider')
+    private iHashProvider: IHashProvider,
+  ) {}
+
+  public async execute({ token, password }: IRequestDTO): Promise<void> {
+    const userToken = await this.userTokensRepository.findByToken(token);
+
+    if (!userToken) {
+      throw new AppError('User token does not exists');
+    }
+
+    const user = await this.usersRepository.findById(userToken.userId);
+
+    if (!user) {
+      throw new AppError('User does not exists');
+    }
+
+    const tokenCreatedAt = userToken.createdAt;
+    const compareDate = addHours(tokenCreatedAt, 2);
+
+    if (isAfter(Date.now(), compareDate)) {
+      throw new AppError('Token expired');
+    }
+
+    user.password = await this.iHashProvider.generateHash(password);
+
+    await this.usersRepository.save(user);
+  }
+}
+
+export default ResetPasswordService;
