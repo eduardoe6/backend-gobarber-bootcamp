fdc686fba564f80246d23e4378a0e65db6173869
eduardoe6 <edueduardo0699@gmail.com>
2021-03-31 16:16:57 -0300

    Criando rotas e controllers referentes ao reset e recuperação de senha do usuário, além do repositório e migration de tokens de usuário.

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,10 +1,16 @@
+import { container } from 'tsyringe';
+
+import '@modules/users/providers';
+import '@shared/container/providers';
+
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
-import '@modules/users/providers';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
-import '@shared/container/providers';
-import { container } from 'tsyringe';
+
+import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
 
 container.registerSingleton<IAppointmentsRepository>(
   'AppointmentsRepository',
@@ -14,4 +20,9 @@ container.registerSingleton<IAppointmentsRepository>(
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
+);
+
+container.registerSingleton<IUserTokensRepository>(
+  'UserTokensRepository',
+  UserTokensRepository,
 );
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,4 +1,5 @@
 import appointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes';
+import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import { Router } from 'express';
@@ -7,6 +8,8 @@ const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
 routes.use('/users', usersRouter);
+
 routes.use('/sessions', sessionsRouter);
+routes.use('/password', passwordRouter);
 
 export default routes;
